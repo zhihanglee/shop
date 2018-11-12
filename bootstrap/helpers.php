5165fd6086506bdfe8120bf39e6339f700d282d1
vagrant <vagrant@homestead>
2018-11-12 10:38:37 +0000

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,5 +1,7 @@
-<?php 
+<?php
 //辅助函数
-function test_helper(){
-    return 'ok';
+function route_class()
+{
+    // 将当前请求的路由名称转换为 CSS 类名称，
+    return str_replace('.', '-', Route::currentRouteName());
 }
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            Lee 设计编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+        <p class="pull-right">
+            <a href="mailto:root@zhihanglee.cn">联系我们</a>
+        </p>
+    </div>
+</footer>
