fdadcd11284013574dd773b05e15855e8b1ca5f0
vagrant <vagrant@homestead>
2018-11-13 09:01:42 +0000

    注册登录初始化

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -16,8 +16,32 @@
             <ul class="nav navbar-nav">
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">登录</a></li>
-                <li><a href="#">注册</a></li>
+                @guest
+                    <li><a href="{{url('login')}}">登录</a></li>
+                    <li><a href="{{url('register')}}">注册</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                        <span class="user-avatar pull-left" style="margin-right: 8px;margin-top: -5px;">
+                             <img
+                                 src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60"
+                                 class="img-responsive img-circle" width="30px" height="30px">
+                        </span>
+                            {{Auth::user()->name}} <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="{{route('logout')}}"
+                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    退出登录
+                                </a>
+                                <form action="{{route('logout')}}" method="post" style="display: none;" id="logout-form">
+                                    {{csrf_field()}}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endguest
             </ul>
         </div>
     </div>
